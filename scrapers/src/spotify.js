7639b63675719470e1f4ccde2464bac65229447c
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-05-23 12:33:05 +0700

    Update spotify.js

--- scrapers/src/spotify.js
+++ scrapers/src/spotify.js
@@ -1,3 +1,13 @@
+/**
+ *  ═══🗡️≪ DEMON CORE ≫🗡️═══
+ *  @ Scrape: [Spotify]
+ *  @ Creator: [Dxyz - Putra]
+ *  @ Name Bot: [Rin-Okumura]
+ *  @ Link Ch: [ https://whatsapp.com/channel/0029Vb6Q4eA1Hsq5qeBu0G1z ]
+ *  @ Note: [Not Hapus Wm]
+ *  ═══🔥≪ 業火 ≫🔥═══
+ **/
+
 const axios = require("axios");
 
 const client_id = "acc6302297e040aeb6e4ac1fbdfd62c3";
@@ -32,6 +42,18 @@ const toTime = (ms) => {
     let m = Math.floor(ms / 60000) % 60;
     let s = Math.floor(ms / 1000) % 60;
     return [h, m, s].map((v) => v.toString().padStart(2, "0")).join(":");
+};
+
+const base64EncodingUrl = (trackUrl, trackName, artistName) => {
+    const data = `__/:${trackUrl}:${trackName}:${artistName}`;
+    const base64Encoded = Buffer.from(data).toString('base64');
+    return base64Encoded
+};
+
+const headers = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36',
+    'Referer': 'https://spotify-down.com/'
 };
 
 class Spotify {
@@ -72,30 +94,51 @@ class Spotify {
         }
     };
 
-    download = async function spotifydl(url) {
-        try {
-            const download = await axios.post(`https://spotydown.media/api/download-track`, {
-                url: url
-            }).then(a => a.data)
-
-            const metadata = await axios.post(`https://spotydown.media/api/get-metadata`, {
-                url: url
-            }).then(a => a.data.apiResponse.data[0])
-
-            const data = {
-                metadata,
-                download
-            }
-
-            return data
-        } catch (e) {
-            return {
-                metadata: e,
-                download: e
-            }
-            console.log(e)
+    download = async function spotifydown(url) {
+        if (!/open.spotify.com/.test(url)) return new Error("Input Url from Spotify !")
+        const result = {
+            status: false,
+            success: 500,
+            metadata: {},
+            download: ''
         }
-    }
+        return new Promise(async (resolve, reject) => {
+            const config = {
+                method: 'post',
+                url: 'https://spotify-down.com/api/metadata',
+                params: {
+                    link: url
+                },
+                ...headers
+            };
+
+            await axios(config).then(async (metadata) => {
+                const base64EncodedT = await base64EncodingUrl(metadata.data.data.link, metadata.data.data.title, metadata.data.data.artists);
+
+                result.status = true || false;
+                result.success = 200 || 500;
+                result.metadata = metadata.data.data || {};
+                result.download = await axios.get('https://spotify-down.com/api/download', {
+                    params: {
+                        link: metadata.data.data.link,
+                        n: metadata.data.data.title,
+                        a: metadata.data.data.artists,
+                        t: base64EncodedT,
+                    },
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36',
+                        'Referer': 'https://spotify-down.com/',
+                    },
+                }).then(a => a.data.data.link) || '';
+            });
+
+            resolve(result);
+        }).catch((e) => {
+            reject({
+                msg: 'Error Gagal Di Download'
+            })
+        })
+    };
 }
 
 module.exports = new Spotify();
